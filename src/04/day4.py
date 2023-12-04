078cefe13e1fd21eaf0fe577d8ff395aa56b1c95
TianLangHin <tianlanghin@gmail.com>
2023-12-04 17:09:49 +1100

    Day 4 Part 2

--- src/04/day4.py
+++ src/04/day4.py
@@ -10,5 +10,22 @@ def part1(filename: str) -> int:
                 s += 1 << (have - 1)
         return s
 
+def part2(filename: str) -> int:
+    with open(filename, 'rt') as f:
+        s = 0
+        bonuses = {}
+        for line in f:
+            card, data = line.split(':')
+            _, card_no = card.split()
+            card_no = int(card_no)
+            wins, have = [x.strip() for x in data.split('|')]
+            wins = [int(x) for x in wins.split()]
+            have = sum(1 for x in have.split() if int(x) in wins)
+            for i in range(1, have+1):
+                bonuses[card_no + i] = bonuses.get(card_no + i, 0) + bonuses.get(card_no, 0) + 1
+            s += bonuses.get(card_no, 0) + 1
+        return s
+
 if __name__ == '__main__':
     print(part1('day4_input.txt'))
+    print(part2('day4_input.txt'))
