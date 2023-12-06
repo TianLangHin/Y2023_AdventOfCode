eafdd0bc5be4b2d8f396ddc7d6659f70bfe6c429
TianLangHin <tianlanghin@gmail.com>
2023-12-06 16:51:48 +1100

    Implemented Day 6 Part 2 in Python

--- src/06/day6.py
+++ src/06/day6.py
@@ -16,5 +16,20 @@ def part1(filename: str) -> int:
                 s *= upper_bound - lower_bound + 1
     return s
 
+def part2(filename: str) -> int:
+    with open(filename, 'rt') as f:
+        t, d = [int(x.split(':')[1].strip().replace(' ', '')) for x in f.readlines()]
+    h_shift = t/2
+    v_shift = t*t/4 - d
+    if v_shift > 0:
+        lower_bound = h_shift - sqrt(v_shift)
+        upper_bound = h_shift + sqrt(v_shift)
+        lower_bound = ceil(lower_bound) + (lower_bound == int(lower_bound))
+        upper_bound = floor(upper_bound) - (upper_bound == int(upper_bound))
+        if upper_bound >= lower_bound:
+            return upper_bound - lower_bound + 1
+    return 0
+
 if __name__ == '__main__':
     print(part1('day6_input.txt'))
+    print(part2('day6_input.txt'))
