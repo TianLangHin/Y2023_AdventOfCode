486aade7d49fd47791e71a4842d334f19b963081
TianLangHin <tianlanghin@gmail.com>
2023-12-07 16:22:08 +1100

    Day 7 Part 1 Implemented in Python

---
+++ src/07/day7.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+
+# hand_type: int, cards: tuple[int,int,int,int,int]
+Hand = namedtuple('Hand', ['hand_type', 'cards'])
+
+def make_type(card_counter: dict[str,int]) -> int:
+    freq = list(card_counter.values())
+    if freq.count(5) == 1:
+        return 6
+    elif freq.count(4) == 1:
+        return 5
+    elif freq.count(3) == 1 and freq.count(2) == 1:
+        return 4
+    elif freq.count(3) == 1 and freq.count(1) == 2:
+        return 3
+    elif freq.count(2) == 2:
+        return 2
+    elif freq.count(2) == 1 and freq.count(1) == 3:
+        return 1
+    return 0
+
+def part1(filename: str) -> int:
+    hands: list[tuple[Hand,int]] = []
+    card_map = {
+        'A': 14,
+        'K': 13,
+        'Q': 12,
+        'J': 11,
+        'T': 10,
+        '9': 9,
+        '8': 8,
+        '7': 7,
+        '6': 6,
+        '5': 5,
+        '4': 4,
+        '3': 3,
+        '2': 2,
+    }
+    with open(filename, 'rt') as f:
+        for line in f:
+            cards, bid = line.split()
+            card_counter = {}
+            card_list = []
+            for card in cards:
+                card_counter[card] = card_counter.get(card, 0) + 1
+                card_list.append(card_map[card])
+            hands.append((Hand(make_type(card_counter), tuple(card_list)), int(bid)))
+    hands.sort(key=lambda x: x[0])
+    return sum((i+1)*bid for i, (_, bid) in enumerate(hands))
+
+def part2(filename: str) -> int:
+    pass
+
+if __name__ == '__main__':
+    print(part1('day7_input.txt'))
+    print(part2('day7_input.txt'))
