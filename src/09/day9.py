ff8ede45002d1b613a07bc893a77c8c48e6f82e3
TianLangHin <tianlanghin@gmail.com>
2023-12-09 16:42:40 +1100

    Day 9 Implemented in Python, using functional patterns)

--- src/09/day9.py
+++ src/09/day9.py
@@ -1,22 +1,26 @@
-def part1(filename: str) -> int:
+from functools import reduce
+
+def get_input(filename: str) -> list[list[int]]:
     values: list[list[int]] = []
     with open(filename, 'rt') as f:
         for line in f:
             values.append([int(x) for x in line.strip().split()])
-    s = 0
-    for numbers in values:
-        n = len(numbers)
-        layers = []
-        while any(x != 0 for x in numbers):
-            layers.append(numbers)
-            numbers = [numbers[i+1] - numbers[i] for i in range(len(numbers)-1)]
-        s += sum(x[-1] for x in layers) + numbers[-1]
-    return s
+    return values
+
+def get_layers(numbers: list[int]) -> list[list[int]]:
+    layers = []
+    while any(x != 0 for x in numbers):
+        layers.append(numbers)
+        numbers = [numbers[i+1] - numbers[i] for i in range(len(numbers)-1)]
+    return layers
+
+def part1(filename: str) -> int:
+    return sum(sum(x[-1] for x in get_layers(numbers)) for numbers in get_input(filename))
 
 def part2(filename: str) -> int:
-    pass
+    diff = lambda itr: reduce(lambda acc, x: x - acc, itr, 0)
+    return sum(diff(x[0] for x in reversed(get_layers(numbers))) for numbers in get_input(filename))
 
 if __name__ == '__main__':
     print(part1('day9_input.txt'))
-    print(part2('test_input.txt'))
-    #print(unique_polynomial([10, 13, 16, 21, 30, 45]))
+    print(part2('day9_input.txt'))
