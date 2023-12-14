75124d617c604d39c59e3c77ca840a70038a121c
TianLangHin <tianlanghin@gmail.com>
2023-12-14 18:00:18 +1100

    Implemented Day 14 Part 2 in Python

--- src/14/day14.py
+++ src/14/day14.py
@@ -2,6 +2,64 @@ from collections import namedtuple
 
 RockData = namedtuple('RockData', ['count', 'sum'])
 
+def one_cycle(grid: list[int], x_bound: int, y_bound: int):
+    # north
+    anchors = [-1] * x_bound
+    for i in range(y_bound):
+        for j in range(x_bound):
+            match grid[i * x_bound + j]:
+                case 1:
+                    grid[i * x_bound + j] = 0
+                    grid[(anchors[j] + 1) * x_bound + j] = 1
+                    anchors[j] += 1
+                case 2:
+                    anchors[j] = i
+    # west
+    anchors = [-1] * y_bound
+    for j in range(x_bound):
+        for i in range(y_bound):
+            match grid[i * x_bound + j]:
+                case 1:
+                    grid[i * x_bound + j] = 0
+                    grid[i * x_bound + (anchors[i] + 1)] = 1
+                    anchors[i] += 1
+                case 2:
+                    anchors[i] = j
+    # south
+    anchors = [x_bound] * x_bound
+    for i in range(y_bound-1, -1, -1):
+        for j in range(x_bound-1, -1, -1):
+            match grid[i * x_bound + j]:
+                case 1:
+                    grid[i * x_bound + j] = 0
+                    grid[(anchors[j] - 1) * x_bound + j] = 1
+                    anchors[j] -= 1
+                case 2:
+                    anchors[j] = i
+    # east
+    anchors = [y_bound] * y_bound
+    for i in range(x_bound-1, -1, -1):
+        for j in range(y_bound-1, -1, -1):
+            match grid[i * x_bound + j]:
+                case 1:
+                    grid[i * x_bound + j] = 0
+                    grid[i * x_bound + (anchors[i] - 1)] = 1
+                    anchors[i] -= 1
+                case 2:
+                    anchors[i] = j
+
+def grid_to_key(grid: list[int]):
+    return sum(grid[i] << (i << 1) for i in range(len(grid)))
+
+def key_to_grid(key: int, total_length: int):
+    grid = []
+    while key != 0:
+        grid.append(key & 3)
+        key >>= 2
+    for _ in range(total_length - len(grid)):
+        grid.append(0)
+    return grid
+
 def part1(filename: str) -> int:
     with open(filename, 'rt') as f:
         lines = [x.strip() for x in f.readlines()]
@@ -27,8 +85,43 @@ def part1(filename: str) -> int:
     )
 
 def part2(filename: str) -> int:
-    return 0
+    with open(filename, 'rt') as f:
+        lines = [x.strip() for x in f.readlines()]
+    x_bound = len(lines[0])
+    y_bound = len(lines)
+    grid = []
+    for line in lines:
+        for character in line:
+            match character:
+                case '#':
+                    grid.append(2)
+                case 'O':
+                    grid.append(1)
+                case '.':
+                    grid.append(0)
+    memo = [grid_to_key(grid)]
+    index = None
+    while True:
+        one_cycle(grid, x_bound, y_bound)
+        new_key = grid_to_key(grid)
+        index = None
+        for i in range(len(memo)):
+            if memo[i] == new_key:
+                index = i
+                break
+        if index is None:
+            memo.append(new_key)
+        else:
+            break
+    final_result = memo[(1_000_000_000 - index) % (len(memo) - index) + index]
+    final_grid = key_to_grid(final_result, x_bound * y_bound)
+    final_load = 0
+    for i in range(y_bound):
+        for j in range(x_bound):
+            if final_grid[i * x_bound + j] == 1:
+                final_load += y_bound - i
+    return final_load
 
 if __name__ == '__main__':
     print(part1('day14_input.txt'))
-    print(part2('test_input.txt'))
+    print(part2('day14_input.txt'))
