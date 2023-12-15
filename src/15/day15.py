9fccc119649c45271ee316617ac77892355e840f
TianLangHin <tianlanghin@gmail.com>
2023-12-15 16:34:11 +1100

    Implemented Day 15 Part 2 in Python

--- src/15/day15.py
+++ src/15/day15.py
@@ -1,13 +1,37 @@
 from functools import reduce
 
+def hash(string: str) -> int:
+    return reduce(lambda acc, e: ((acc + ord(e)) * 17) & 255, string, 0)
+
 def part1(filename: str) -> int:
     with open(filename, 'rt') as f:
         text = f.readlines()[0].strip()
     strings = text.split(',')
-    return sum(
-        reduce(lambda acc, e: ((acc + ord(e)) * 17) % 256, string, 0)
+    return sum(hash(string) for string in strings)
+
+def part2(filename: str) -> int:
+    with open(filename, 'rt') as f:
+        text = f.readlines()[0].strip()
+    strings = text.split(',')
+    operations = [
+        (string[:-1], 0) if string.endswith('-')
+        else [(a, int(b)) for a, b in [string.split('=')]][0]
         for string in strings
+    ]
+    boxes = [dict() for _ in range(256)]
+    for string, focal_length in operations:
+        index = hash(string)
+        if focal_length == 0:
+            existing_lens = boxes[index].get(string, None)
+            if existing_lens is not None:
+                boxes[index].pop(string)
+        else:
+            boxes[index][string] = focal_length
+    return sum(
+        (i+1) * sum((j+1) * f for j, (_, f) in enumerate(boxes[i].items()))
+        for i in range(256)
     )
 
 if __name__ == '__main__':
     print(part1('day15_input.txt'))
+    print(part2('day15_input.txt'))
