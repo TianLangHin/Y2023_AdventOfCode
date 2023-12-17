b26423fc922e1abe536a017f8ed951079e7b6079
TianLangHin <tianlanghin@gmail.com>
2023-12-17 20:37:52 +1100

    Streamlined common logic between Parts 1 and 2 in Python Day 17 implementation

--- src/17/day17.py
+++ src/17/day17.py
@@ -10,6 +10,16 @@ def in_bound(direction: int, pos: int, x_bound: int, y_bound: int) -> bool:
     elif direction == UP:
         return pos // x_bound > 0
 
+def increment(index: int, direction: int, x_bound: int) -> int:
+    if direction == RIGHT:
+        return index + 1
+    elif direction == LEFT:
+        return index - 1
+    elif direction == DOWN:
+        return index + x_bound
+    elif direction == UP:
+        return index - x_bound
+
 # `count` is [0-3] << 2 for Part 1
 # two bits for `count`.
 def part1(filename: str) -> int:
@@ -21,8 +31,6 @@ def part1(filename: str) -> int:
     max_cost = 9 * (x_bound + y_bound)
     costs = [max_cost] * (len(grid) * 4 * 4)
     stopping_point = len(grid) - 1
-    increment = lambda idx, d: idx + ((
-        1 if d == RIGHT else -1 if d == LEFT else -x_bound if d == UP else x_bound) << 4)
     # index is represented as: [index][count][direction]
     costs[0] = 0
     paths = {0}
@@ -33,7 +41,7 @@ def part1(filename: str) -> int:
                 if in_bound(search_direction, index >> 4, x_bound, y_bound):
                     if index == 0:
                         # ignore count, store new direction
-                        i = increment(0, search_direction) | 4 | search_direction
+                        i = (increment(0, search_direction, x_bound) << 4) | 4 | search_direction
                         new_cost = costs[index] + grid[i >> 4]
                         if new_cost >= costs[i]:
                             continue
@@ -43,7 +51,7 @@ def part1(filename: str) -> int:
                     elif search_direction == (index & 3): # same direction
                         if (index & 12) != 12: # count is not 3 (represented as 12)
                             # count + 1
-                            i = increment(index, search_direction) + 4
+                            i = (increment(index >> 4, search_direction, x_bound) << 4) + (index & 15) + 4
                             new_cost = costs[index] + grid[i >> 4]
                             if new_cost >= costs[i]:
                                 continue
@@ -53,7 +61,7 @@ def part1(filename: str) -> int:
                     # switching direction, set count to 0, cannot reverse
                     elif search_direction + (index & 3) != 3:
                         # ignore count, store new direction
-                        i = ((increment(index, search_direction) >> 4) << 4) | 4 | search_direction
+                        i = (increment(index >> 4, search_direction, x_bound) << 4) | 4 | search_direction
                         new_cost = costs[index] + grid[i >> 4]
                         if new_cost >= costs[i]:
                             continue
@@ -74,8 +82,6 @@ def part2(filename: str) -> int:
     max_cost = 9 * (x_bound + y_bound)
     costs = [max_cost] * (len(grid) * 4 * 16)
     stopping_point = len(grid) - 1
-    increment = lambda idx, d: idx + ((
-        1 if d == RIGHT else -1 if d == LEFT else -x_bound if d == UP else x_bound) << 6)
     # index is represented as: [index][count][direction]
     costs[0] = 0
     paths = {0}
@@ -86,7 +92,7 @@ def part2(filename: str) -> int:
                 if in_bound(search_direction, index >> 6, x_bound, y_bound):
                     if index == 0:
                         # ignore count, store new direction
-                        i = increment(0, search_direction) | 4 | search_direction
+                        i = (increment(0, search_direction, x_bound) << 6) | 4 | search_direction
                         new_cost = costs[index] + grid[i >> 6]
                         if new_cost >= costs[i]:
                             continue
@@ -96,7 +102,7 @@ def part2(filename: str) -> int:
                     elif search_direction == (index & 3): # same direction
                         if (index & 60) < 40: # count is <= 10 (represented as 40)
                             # count + 1
-                            i = increment(index, search_direction) + 4
+                            i = (increment(index >> 6, search_direction, x_bound) << 6) + (index & 63) + 4
                             new_cost = costs[index] + grid[i >> 6]
                             if new_cost >= costs[i]:
                                 continue
@@ -110,7 +116,7 @@ def part2(filename: str) -> int:
                     elif search_direction + (index & 3) != 3:
                         if (index & 60) >= 16: # count is >= 4 (represented as 16)
                             # ignore count, store new direction
-                            i = ((increment(index, search_direction) >> 6) << 6) | 4 | search_direction
+                            i = (increment(index >> 6, search_direction, x_bound) << 6) | 4 | search_direction
                             new_cost = costs[index] + grid[i >> 6]
                             if new_cost >= costs[i]:
                                 continue
