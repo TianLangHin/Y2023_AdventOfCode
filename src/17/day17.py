52aef33cd70dac7505eccdb5a86ea676407396c6
TianLangHin <tianlanghin@gmail.com>
2023-12-17 20:20:25 +1100

    Implemented Day 17 Part 2 in Python

--- src/17/day17.py
+++ src/17/day17.py
@@ -1,5 +1,4 @@
 RIGHT, DOWN, UP, LEFT = 0, 1, 2, 3
-# count is 0, 4, 8, 12
 
 def in_bound(direction: int, pos: int, x_bound: int, y_bound: int) -> bool:
     if direction == RIGHT:
@@ -11,7 +10,14 @@ def in_bound(direction: int, pos: int, x_bound: int, y_bound: int) -> bool:
     elif direction == UP:
         return pos // x_bound > 0
 
-def compute_min_path(grid: list[int], x_bound: int, y_bound: int) -> int:
+# `count` is [0-3] << 2 for Part 1
+# two bits for `count`.
+def part1(filename: str) -> int:
+    with open(filename, 'rt') as f:
+        text_lines = [x.strip() for x in f.readlines()]
+    x_bound = len(text_lines[0])
+    y_bound = len(text_lines)
+    grid = [int(x) for line in text_lines for x in line]
     max_cost = 9 * (x_bound + y_bound)
     costs = [max_cost] * (len(grid) * 4 * 4)
     stopping_point = len(grid) - 1
@@ -57,13 +63,63 @@ def compute_min_path(grid: list[int], x_bound: int, y_bound: int) -> int:
         paths = new_paths
     return min(costs[stopping_point << 4 : (stopping_point << 4) + 16])
 
-def part1(filename: str) -> int:
+# `count` is [0-10] << 2 for Part 1
+# four bits for `count`.
+def part2(filename: str) -> int:
     with open(filename, 'rt') as f:
         text_lines = [x.strip() for x in f.readlines()]
     x_bound = len(text_lines[0])
     y_bound = len(text_lines)
     grid = [int(x) for line in text_lines for x in line]
-    return compute_min_path(grid, x_bound, y_bound)
+    max_cost = 9 * (x_bound + y_bound)
+    costs = [max_cost] * (len(grid) * 4 * 16)
+    stopping_point = len(grid) - 1
+    increment = lambda idx, d: idx + ((
+        1 if d == RIGHT else -1 if d == LEFT else -x_bound if d == UP else x_bound) << 6)
+    # index is represented as: [index][count][direction]
+    costs[0] = 0
+    paths = {0}
+    while paths:
+        new_paths = set()
+        for index in paths:
+            for search_direction in range(4):
+                if in_bound(search_direction, index >> 6, x_bound, y_bound):
+                    if index == 0:
+                        # ignore count, store new direction
+                        i = increment(0, search_direction) | 4 | search_direction
+                        new_cost = costs[index] + grid[i >> 6]
+                        if new_cost >= costs[i]:
+                            continue
+                        costs[i] = new_cost
+                        if (i >> 6) != stopping_point:
+                            new_paths.add(i)
+                    elif search_direction == (index & 3): # same direction
+                        if (index & 60) < 40: # count is <= 10 (represented as 40)
+                            # count + 1
+                            i = increment(index, search_direction) + 4
+                            new_cost = costs[index] + grid[i >> 6]
+                            if new_cost >= costs[i]:
+                                continue
+                            if (i >> 6) == stopping_point:
+                                if (i & 60) >= 16: # count is >= 4 (represented as 16)
+                                    costs[i] = new_cost
+                            else:
+                                costs[i] = new_cost
+                                new_paths.add(i)
+                    # switching direction, set count to 0, cannot reverse
+                    elif search_direction + (index & 3) != 3:
+                        if (index & 60) >= 16: # count is >= 4 (represented as 16)
+                            # ignore count, store new direction
+                            i = ((increment(index, search_direction) >> 6) << 6) | 4 | search_direction
+                            new_cost = costs[index] + grid[i >> 6]
+                            if new_cost >= costs[i]:
+                                continue
+                            if (i >> 6) != stopping_point:
+                                costs[i] = new_cost
+                                new_paths.add(i)
+        paths = new_paths
+    return min(costs[stopping_point << 6 : (stopping_point << 6) + 64])
 
 if __name__ == '__main__':
     print(part1('day17_input.txt'))
+    print(part2('day17_input.txt'))
